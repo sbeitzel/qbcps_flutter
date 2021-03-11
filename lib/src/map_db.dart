9ef7cca161eb9604dd6c0bffa06766157a57956c
Stephen Beitzel <sbeitzel@pobox.com>
2021-03-11 12:58:20 -0800

    Update library for null safety

--- lib/src/map_db.dart
+++ lib/src/map_db.dart
@@ -14,10 +14,11 @@ import 'package:tuple/tuple.dart';
 class MapDB<K, V> {
   final Map<K, V> _map = {};
   // ignore: close_sinks
-  final StreamController<Tuple2<K, V>> _streamController =
-      StreamController<Tuple2<K, V>>.broadcast();
+  final StreamController<Tuple2<K, V?>> _streamController =
+      StreamController<Tuple2<K, V?>>.broadcast();
   // ignore: close_sinks
-  final StreamController<List<V>> _valuesController = StreamController<List<V>>.broadcast();
+  final StreamController<List<V>> _valuesController =
+      StreamController<List<V>>.broadcast();
 
   /// Constructs an empty MapDB.
   MapDB() {
@@ -32,10 +33,10 @@ class MapDB<K, V> {
   /// The previous value for [key], if any, is returned. If there was no
   /// previous value -- that is, if this is an INSERT rather than an UPDATE --
   /// then the return value is `null`.
-  V put(K key, V value) {
-    V oldValue = _map[key];
+  V? put(K key, V value) {
+    var oldValue = _map[key];
     _map[key] = value;
-    _streamController.add(Tuple2<K, V>(key, value));
+    _streamController.add(Tuple2<K, V?>(key, value));
     return oldValue;
   }
 
@@ -43,9 +44,9 @@ class MapDB<K, V> {
   ///
   /// The previous value for [key], if any, is returned. If there was no
   /// previous value then the return value is `null`.
-  V delete(K key) {
-    V rv = _map.remove(key);
-    _streamController.add(Tuple2<K, V>(key, null));
+  V? delete(K key) {
+    var rv = _map.remove(key);
+    _streamController.add(Tuple2<K, V?>(key, null));
     return rv;
   }
 
@@ -53,7 +54,7 @@ class MapDB<K, V> {
   /// this method will return `null` rather than throw an exception.
   ///
   /// See [Map.[]].
-  V get(K key) {
+  V? get(K key) {
     return _map[key];
   }
 
@@ -74,7 +75,7 @@ class MapDB<K, V> {
   ///
   /// All modifications to the database contents will be emitted by the
   /// returned [Stream].
-  Stream<Tuple2<K, V>> getChangeStream() {
+  Stream<Tuple2<K, V?>> getChangeStream() {
     return _streamController.stream;
   }
 
@@ -83,14 +84,14 @@ class MapDB<K, V> {
   /// The first event on the returned [Stream] will be the current value
   /// stored at [key]. Whenever a new value is stored at [key], that value
   /// will be sent out on the returned stream.
-  Stream<V> getChangesForKey(K key) {
+  Stream<V?> getChangesForKey(K key) {
     // ignore: close_sinks
-    StreamController<V> changeController = StreamController<V>();
+    var changeController = StreamController<V?>();
     changeController.add(_map[key]);
     changeController
-        .addStream(_streamController.stream.where((Tuple2<K, V> tuple) {
+        .addStream(_streamController.stream.where((Tuple2<K, V?> tuple) {
       return key == tuple.item1;
-    }).map<V>((Tuple2<K, V> tuple) => tuple.item2));
+    }).map<V?>((Tuple2<K, V?> tuple) => tuple.item2));
     return changeController.stream;
   }
 }
